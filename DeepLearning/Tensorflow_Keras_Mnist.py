211e04ed20a18657e8fb1025449c2fe85b4caba1
andrew <andrew.ragone@gmail.com>
2017-03-03 23:16:39 -0500

    Add Layers

--- DeepLearning/Tensorflow_Keras_Mnist.py
+++ DeepLearning/Tensorflow_Keras_Mnist.py
@@ -6,7 +6,8 @@ plt.rcParams['figure.figsize'] = (7,7) # Make the figures a bit bigger
 
 from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.utils import np_utils
 
 
@@ -26,8 +27,8 @@ def run():
         #    plt.title("Class {}".format(y_train[i]))
         #plt.show()
 
-        X_train = X_train.reshape(60000, 784)
-        X_test = X_test.reshape(10000, 784)
+        X_train = X_train.reshape(60000, 1, 28, 28)
+        X_test = X_test.reshape(10000, 1, 28, 28)
         X_train = X_train.astype('float32')
         X_test = X_test.astype('float32')
         X_train /= 255
@@ -42,20 +43,20 @@ def run():
 
 
         model = Sequential()
-        model.add(Dense(512, input_shape=(784,)))
-        model.add(Activation('relu'))   # An "activation" is just a non-linear function applied to the output
-                                        # of the layer above. Here, with a "rectified linear unit",
-                                        # we clamp all values below 0 to 0.
+        model.add(Convolution2D(32, nb_row=1, nb_col=1, input_shape=(1, 28, 28,)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(1, 1)))
 
-        model.add(Dropout(0.2))  # Dropout helps protect the model from memorizing or "overfitting" the training data
+        model.add(Convolution2D(32, 1, 1))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(1, 1)))
+
+        model.add(Flatten())
         model.add(Dense(512))
         model.add(Activation('relu'))
-        model.add(Dropout(0.2))
+        model.add(Dropout(0.5))  # Dropout helps protect the model from memorizing or "overfitting" the training data
         model.add(Dense(10))
-        model.add(Activation('softmax'))    # This special "softmax" activation among other things,
-                                            # ensures the output is a valid probaility distribution, that is
-                                            # that its values are all non-negative and sum to 1.
-
+        model.add(Activation('softmax'))
         model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=["accuracy"])
         #model.compile(loss='categorical_crossentropy', optimizer='adam')
 
@@ -64,7 +65,7 @@ def run():
         for i in range(0, 60000, step_size):
             sub_x = X_train[i:(step_size+i)]
             sub_y = Y_train[i:(step_size+i)]
-            sub_x = sub_x.reshape(step_size, 784)
+            sub_x = sub_x.reshape(step_size, 1, 28, 28)
             sub_y = sub_y.reshape(step_size, 10)
             model.fit(sub_x, sub_y, nb_epoch=1, verbose=0)
 
